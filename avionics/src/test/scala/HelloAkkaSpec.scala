1f2427f0aba1c124c293c4c1c902ca047424889b
Mozart Brocchini <Mozart.Brocchini@pason.com>
2015-03-13 05:55:08 -0500

    finished chapter 5

---
+++ avionics/src/test/scala/HelloAkkaSpec.scala
@@ -0,0 +1,5 @@
+import org.scalatest.{ BeforeAndAfterAll, FlatSpecLike, Matchers }
+import akka.actor.{ Actor, Props, ActorSystem }
+import akka.testkit.{ ImplicitSender, TestKit, TestActorRef }
+import scala.concurrent.duration._
+
